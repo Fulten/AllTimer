c651769903bfa75182bdca2b2428ed213d7e66eb
HWogi <HowardW323@gmail.com>
2024-10-17 14:30:05 -0400

    Adding UI sound effects, main menu controls, and a background texture

---
+++ VBoxContainer.gd
@@ -0,0 +1,22 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$StartButton.grab_focus()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_start_button_pressed():
+	pass # Replace with function body.
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+

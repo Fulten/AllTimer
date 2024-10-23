c541e4dead7c5fd82f02bbe2db4c32a642d6421e
Dome <jjreinholdt@gmail.com>
2024-10-23 16:04:30 -0500

    Updated questions to do new lines on save to json for easier merges -Updated to follow correct and 3 wrong answers instead of assigned answers

--- Question_editor.gd
+++ Question_editor.gd
@@ -5,26 +5,24 @@ var json_data = {
 }
 
 var question_content = {
-	"name": "enter name here",
-	"question": "enter question here",
-	"a": "enter answer a",
-	"b": "enter answer b",
-	"c": "enter answer c",
-	"d": "enter answer d",
-	"correct": "enter correct selection",
-	"explainer": "enter post prompt text or clear",
-	"tags": "enter , delimited list of tags for this question"
+	"name": "",
+	"question": "",
+	"correct": "",
+	"wrong_1": "",
+	"wrong_2": "",
+	"wrong_3": "",
+	"explainer": "",
+	"tags": ""
 }
 
 @onready var questions_container = $VBoxContainer/Questions
 @onready var popup_dialog = $Popup
 @onready var question_name = $Popup/Name
 @onready var question_text = $Popup/Text
-@onready var question_answer_a = $Popup/a
-@onready var question_answer_b = $Popup/b
-@onready var question_answer_c = $Popup/c
-@onready var question_answer_d = $Popup/d
 @onready var question_answer_correct = $Popup/correct
+@onready var question_answer_wrong1 = $Popup/wrong1
+@onready var question_answer_wrong2 = $Popup/wrong2
+@onready var question_answer_wrong3 = $Popup/wrong3
 @onready var question_post_prompt = $Popup/prompt
 @onready var question_tags = $Popup/tags
 @onready var add_button = $"VBoxContainer/Add Question"
@@ -46,11 +44,10 @@ func _on_add_question_button_pressed():
 	editing_index = -1
 	question_name.text = question_content["name"]
 	question_text.text = question_content["question"]
-	question_answer_a.text = question_content["a"]
-	question_answer_b.text = question_content["b"]
-	question_answer_c.text = question_content["c"]
-	question_answer_d.text = question_content["d"]
 	question_answer_correct.text = question_content["correct"]
+	question_answer_wrong1.text = question_content["wrong_1"]
+	question_answer_wrong2.text = question_content["wrong_2"]
+	question_answer_wrong3.text = question_content["wrong_3"]
 	question_post_prompt.text = question_content["explainer"]
 	question_tags.text = question_content["tags"]
 	popup_dialog.popup()
@@ -60,11 +57,10 @@ func _on_edit_question_button_pressed():
 	if json_data["questions"].size() > 0:
 		question_name.text = json_data["questions"][editing_index]["name"]
 		question_text.text = json_data["questions"][editing_index]["question"]
-		question_answer_a.text = json_data["questions"][editing_index]["a"]
-		question_answer_b.text = json_data["questions"][editing_index]["b"]
-		question_answer_c.text = json_data["questions"][editing_index]["c"]
-		question_answer_d.text = json_data["questions"][editing_index]["d"]
 		question_answer_correct.text = json_data["questions"][editing_index]["correct"]
+		question_answer_wrong1.text = json_data["questions"][editing_index]["wrong_1"]
+		question_answer_wrong2.text = json_data["questions"][editing_index]["wrong_2"]
+		question_answer_wrong3.text = json_data["questions"][editing_index]["wrong_3"]
 		question_post_prompt.text = json_data["questions"][editing_index]["explainer"]
 		question_tags.text = json_data["questions"][editing_index]["tags"]
 		popup_dialog.popup()
@@ -79,11 +75,10 @@ func _on_save_button_pressed():
 	var new_question = question_content.duplicate()
 	new_question["name"] = question_name.text
 	new_question["question"] = question_text.text
-	new_question["a"] = question_answer_a.text
-	new_question["b"] = question_answer_b.text
-	new_question["c"] = question_answer_c.text
-	new_question["d"] = question_answer_d.text
 	new_question["correct"] = question_answer_correct.text
+	new_question["wrong_1"] = question_answer_wrong1.text
+	new_question["wrong_2"] = question_answer_wrong2.text
+	new_question["wrong_3"] = question_answer_wrong3.text
 	new_question["explainer"] = question_post_prompt.text
 	new_question["tags"] = question_tags.text
 	if editing_index == -1:
@@ -96,7 +91,7 @@ func _on_save_button_pressed():
 	popup_dialog.hide()
 
 func _save_questions():
-	var json_string = JSON.stringify(json_data)
+	var json_string = JSON.stringify(json_data, "\t")
 	var file = FileAccess.open("res://question_data.json", FileAccess.WRITE)
 	file.store_string(json_string)
 	file.close()

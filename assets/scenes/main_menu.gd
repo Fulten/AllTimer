721e58d4ecbf8a52869766776e89649ccac9faff
HWogi <HowardW323@gmail.com>
2025-04-07 15:04:04 -0400

    Menu 2.0

--- assets/scenes/main_menu.gd
+++ assets/scenes/main_menu.gd
@@ -1,13 +1,131 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+	pass
+	$Stack_0/MainMenuButtons/PlayButton.grab_focus()
+func _process(_delta):
 	pass
 
+func _input(event):
+	if event.is_action_pressed("1_P"):
+		GameState.PlayerCount = 1
+	elif event.is_action_pressed("2_P"):
+		GameState.PlayerCount = 2
+	elif event.is_action_pressed("3_P"):
+		GameState.PlayerCount = 3
+	elif event.is_action_pressed("4_P"):
+		GameState.PlayerCount = 4
+
+#Button UX
+func _on_play_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_play_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_play_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_play_button_button_up():
+	get_tree().change_scene_to_file("res://assets/scenes/loading_screen.tscn")
+
+func _on_options_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_options_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_options_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_options_button_button_up():
+	get_node("Stack_0").hide()
+	get_node("Options_2").show()
+	$Options_2/OptionsCategories/DisplayButton.grab_focus()
+
+func _on_quit_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_quit_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_quit_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_quit_button_button_up():
+	get_tree().quit()
+
+
+func _on_options_categories_return_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_options_categories_return_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_options_categories_return_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_options_categories_return_button_up():
+	get_node("Options_2").hide()
+	get_node("Stack_0").show()
+	$Stack_0/MainMenuButtons/PlayButton.grab_focus()
+
+func _on_display_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_display_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_display_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_display_button_button_up():
+	get_node("Options_2").hide()
+	get_node("Options_Display2").show()
+
+func _on_sound_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_sound_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_sound_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_sound_button_button_up():
+	get_node("Options_2").hide()
+	get_node("Options_Sound2").show()
+
+func _on_game_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_game_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_game_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_game_button_button_up():
+	get_node("Options_2").hide()
+	get_node("Options_Game2").show()
+
+
+func _on_options_display_return_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_options_display_return_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_options_display_return_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_options_display_return_button_up():
+	get_node("Options_Display2").hide()
+	get_node("Options_2").show()
+	$Options_2/OptionsCategories/DisplayButton.grab_focus()
 	
+func _on_display_list_item_selected(index: int)->void:
+	match index:
+		0:DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		1:DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		2:DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+
+
+func _on_options_sound_return_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_options_sound_return_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_options_sound_return_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_options_sound_return_button_up():
+	get_node("Options_Sound2").hide()
+	get_node("Options_2").show()
+	$Options_2/OptionsCategories/DisplayButton.grab_focus()
+
+func _on_options_game_return_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_options_game_return_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_options_game_return_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_options_game_return_button_up():
+	get_node("Options_Game2").hide()
+	get_node("Options_2").show()
+	$Options_2/OptionsCategories/DisplayButton.grab_focus()
--- assets/uiux/main_menu/VBoxContainer.gd
+++ assets/uiux/main_menu/VBoxContainer.gd
@@ -1,21 +1,22 @@
 extends VBoxContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$StartButton.grab_focus()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
 
-func _input(event):
-	if event.is_action_pressed("1_P"):
-		GameState.PlayerCount = 1
-	elif event.is_action_pressed("2_P"):
-		GameState.PlayerCount = 2
-	elif event.is_action_pressed("3_P"):
-		GameState.PlayerCount = 3
-	elif event.is_action_pressed("4_P"):
-		GameState.PlayerCount = 4
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+#func _input(event):
+#	if event.is_action_pressed("1_P"):
+#		GameState.PlayerCount = 1
+#	elif event.is_action_pressed("2_P"):
+#		GameState.PlayerCount = 2
+#	elif event.is_action_pressed("3_P"):
+#		GameState.PlayerCount = 3
+#	elif event.is_action_pressed("4_P"):
+#		GameState.PlayerCount = 4
+
 func _on_start_button_mouse_entered():
 	$HoverSFX.play()
 func _on_start_button_focus_entered():

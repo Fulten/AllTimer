23c9f93f14e27d6c51aa9177f959ca4c45c5604c
HWogi <HowardW323@gmail.com>
2025-04-16 20:49:10 -0400

    1st-pass node transitions for main menu. Removed unused nodes from main_menu.gd

--- assets/scenes/main_menu.gd
+++ assets/scenes/main_menu.gd
@@ -1,8 +1,8 @@
 extends Control
 
 func _ready():
-	pass
-	$Stack_0/MainMenuButtons/PlayButton.grab_focus()
+	$StackAnimator.play("Anim_Stack0_Init")
+	$Stack_0/TitleHeader2.grab_focus()
 func _process(_delta):
 	pass
 
@@ -33,9 +33,13 @@ func _on_options_button_mouse_entered():
 func _on_options_button_button_down():
 	$Stack_0/MainMenuButtons/SFX_Press.play()
 func _on_options_button_button_up():
-	get_node("Stack_0").hide()
-	get_node("Options_2").show()
-	$Options_2/OptionsCategories/DisplayButton.grab_focus()
+	$StackAnimator/Timer_Stack0_to_Options.start()
+	$StackAnimator.play("Anim_Stack0_FadeOut")
+func _on_timer_stack_0_to_options_categories_timeout():
+		$StackAnimator.play("Anim_OptionsCategories_FadeIn")
+		get_node("Stack_0").hide()
+		get_node("Options_2").show()	
+		$Options_2/OptionsCategories/DisplayButton.grab_focus()
 
 func _on_quit_button_focus_entered():
 	$Stack_0/MainMenuButtons/SFX_Hover.play()
@@ -54,6 +58,10 @@ func _on_options_categories_return_mouse_entered():
 func _on_options_categories_return_button_down():
 	$Stack_0/MainMenuButtons/SFX_Press.play()
 func _on_options_categories_return_button_up():
+	$StackAnimator/Timer_Options_to_Stack0.start()
+	$StackAnimator.play("Anim_OptionCategories_FadeOut")
+func _on_timer_options_categories_to_stack_0_timeout():
+	$StackAnimator.play("Anim_Stack0_FadeIn")
 	get_node("Options_2").hide()
 	get_node("Stack_0").show()
 	$Stack_0/MainMenuButtons/PlayButton.grab_focus()
@@ -65,6 +73,10 @@ func _on_display_button_mouse_entered():
 func _on_display_button_button_down():
 	$Stack_0/MainMenuButtons/SFX_Press.play()
 func _on_display_button_button_up():
+	$StackAnimator/Timer_Options_to_Display.start()
+	$StackAnimator.play("Anim_OptionCategories_FadeOut")
+func _on_timer_options_to_display_timeout():
+	$StackAnimator.play("Anim_Display_FadeIn")
 	get_node("Options_2").hide()
 	get_node("Options_Display2").show()
 
@@ -75,6 +87,10 @@ func _on_sound_button_mouse_entered():
 func _on_sound_button_button_down():
 	$Stack_0/MainMenuButtons/SFX_Press.play()
 func _on_sound_button_button_up():
+	$StackAnimator/Timer_Options_to_Sound.start()
+	$StackAnimator.play("Anim_OptionCategories_FadeOut")
+func _on_timer_options_to_sound_timeout():
+	$StackAnimator.play("Anim_Sound_FadeIn")
 	get_node("Options_2").hide()
 	get_node("Options_Sound2").show()
 
@@ -85,6 +101,10 @@ func _on_game_button_mouse_entered():
 func _on_game_button_button_down():
 	$Stack_0/MainMenuButtons/SFX_Press.play()
 func _on_game_button_button_up():
+	$StackAnimator/Timer_Options_to_Game.start()
+	$StackAnimator.play("Anim_OptionCategories_FadeOut")
+func _on_timer_options_to_game_timeout():
+	$StackAnimator.play("Anim_Game_FadeIn")
 	get_node("Options_2").hide()
 	get_node("Options_Game2").show()
 
@@ -96,8 +116,12 @@ func _on_options_display_return_mouse_entered():
 func _on_options_display_return_button_down():
 	$Stack_0/MainMenuButtons/SFX_Press.play()
 func _on_options_display_return_button_up():
+	$StackAnimator/Timer_Display_to_Options.start()
+	$StackAnimator.play("Anim_Display_FadeOut")
+func _on_timer_display_to_options_timeout():
 	get_node("Options_Display2").hide()
 	get_node("Options_2").show()
+	$StackAnimator.play("Anim_OptionsCategories_FadeIn")	
 	$Options_2/OptionsCategories/DisplayButton.grab_focus()
 	
 func _on_display_list_item_selected(index: int)->void:
@@ -115,9 +139,13 @@ func _on_options_sound_return_mouse_entered():
 func _on_options_sound_return_button_down():
 	$Stack_0/MainMenuButtons/SFX_Press.play()
 func _on_options_sound_return_button_up():
+	$StackAnimator/Timer_Sound_to_Options.start()
+	$StackAnimator.play("Anim_Sound_FadeOut")
+func _on_timer_sound_to_options_timeout():
+	$StackAnimator.play("Anim_OptionsCategories_FadeIn")
 	get_node("Options_Sound2").hide()
 	get_node("Options_2").show()
-	$Options_2/OptionsCategories/DisplayButton.grab_focus()
+	$Options_2/OptionsCategories/SoundButton.grab_focus()
 
 func _on_options_game_return_focus_entered():
 	$Stack_0/MainMenuButtons/SFX_Hover.play()
@@ -126,6 +154,11 @@ func _on_options_game_return_mouse_entered():
 func _on_options_game_return_button_down():
 	$Stack_0/MainMenuButtons/SFX_Press.play()
 func _on_options_game_return_button_up():
+	$StackAnimator/Timer_Game_to_Options.start()
+	$StackAnimator.play("Anim_Game_FadeOut")
+func _on_timer_game_to_options_timeout():
+	$StackAnimator.play("Anim_OptionsCategories_FadeIn")
 	get_node("Options_Game2").hide()
 	get_node("Options_2").show()
-	$Options_2/OptionsCategories/DisplayButton.grab_focus()
+	$Options_2/OptionsCategories/GameButton.grab_focus()
+

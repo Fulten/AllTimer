ee769aa28933c3b6f1ed2e716f5a48eba3f12f9c
HWogi <HowardW323@gmail.com>
2025-06-19 10:47:30 -0400

    Added multiplayer lobby with profile switcher. Added stat-tracker fields to profile submenu.

---
+++ assets/scenes/multiplayer_lobby.gd
@@ -0,0 +1,66 @@
+extends Control
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	pass
+
+
+func _on_host_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_host_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_host_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_host_button_button_up():
+	get_node("PeerConnectors").hide()
+	get_node("HostingLabel").show()
+	get_node("CancelConnectionButton").show()
+
+
+func _on_join_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_join_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_join_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_join_button_button_up():
+	get_node("PeerConnectors").hide()
+	get_node("JoinedLabel").show()
+	get_node("CancelConnectionButton").show()
+
+
+func _on_cancel_connection_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_cancel_connection_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_cancel_connection_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_cancel_connection_button_button_up():
+	get_node("HostingLabel").hide()
+	get_node("JoinedLabel").hide()
+	get_node("CancelConnectionButton").hide()
+	get_node("PeerConnectors").show()
+	
+	
+func _on_launch_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_launch_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_launch_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_launch_button_button_up():
+	pass # Replace with function body.
+
+
+func _on_back_to_main_button_mouse_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_back_to_main_button_focus_entered():
+	$Stack_0/MainMenuButtons/SFX_Hover.play()
+func _on_back_to_main_button_button_down():
+	$Stack_0/MainMenuButtons/SFX_Press.play()
+func _on_back_to_main_button_button_up():
+	get_tree().change_scene_to_file("res://assets/scenes/main_menu.tscn"	)

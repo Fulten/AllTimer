04038359ca4d709c35c910a34e4e0cc4b7b5bcef
Fultan <zathera5277@gmail.com>
2025-07-10 07:27:51 -0500

    added new files for user profiles User_Profile_Utils.gd containing utility functions for managing profiles and io should probably just make it a global

---
+++ assets/scenes/User_Profile_Utils.gd
@@ -0,0 +1,65 @@
+extends Node
+
+
+var UserProfiles = {}
+
+
+func _new_profile(profileName):
+	var newID = 0
+	if UserProfiles != null:
+		for key in UserProfiles.keys():
+			if UserProfiles[key]["id"] >= newID:
+				newID = UserProfiles[key]["id"] + 1
+				pass
+			pass
+		pass
+	
+	var newProfile = {
+		"name": profileName,
+		"id": newID
+	}
+	
+	return newProfile
+	
+func _save_profile(newProfile):
+	if !UserProfiles.has(newProfile.name):
+		print("!INFO: Saving New Profile: [%s]" % newProfile.name)
+		UserProfiles[newProfile.name] = newProfile
+		_IO_write_profiles()
+		pass
+	else:
+		print("!INFO: Profile Name Collision [%s]" % newProfile.name)
+		pass
+	pass
+	
+func _delete_profile(profileName):
+	print("!INFO: Deleting Existing Profile: [%s]" % profileName)
+	UserProfiles.erase(profileName)
+	pass
+
+func _IO_read_profiles():
+	var file = FileAccess.open("res://data/user_profiles.json", FileAccess.READ)
+	if file:
+		UserProfiles = JSON.parse_string(file.get_as_text())
+		file.close()
+		pass
+	else:
+		print("!!ERROR: Failed to read profiles at _read_profiles")
+		pass
+		
+	if UserProfiles == null:
+		UserProfiles = {}
+		pass
+	
+
+func _IO_write_profiles():
+	var file = FileAccess.open("res://data/user_profiles.json", FileAccess.WRITE)
+	if file:
+		var jsonString = JSON.stringify(UserProfiles)
+		file.store_string(jsonString)
+		file.close()
+		pass
+	else:
+		print("!!ERROR: Failed to save profile at _save_profile")
+		pass
+	pass
